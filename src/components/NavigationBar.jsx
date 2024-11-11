d5f2b28c805e68a871087560cfba3f372053cb85
odin <139291798+minecrafter021345@users.noreply.github.com>
2024-11-11 12:49:17 -0800

    Change navigation bar layout

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,16 +1,30 @@
 import '../styles/NavigationBar.css'
 import { Link } from 'react-router-dom'
 
+function Categories() {
+    return (
+        <div className='categories'>
+            <Link>Electronics</Link>
+            <Link>Jewelery</Link>
+            <Link>Men's Clothing</Link>
+            <Link>Women's Clothing</Link>
+        </div>
+    )
+}
+
 export default function NavigationBar({itemQuantity}) {
     return (
         <div className="NavigationBar">
             <Link to='/'>
                 <p className='title'>Dullmart</p>
             </Link>
-            <Link className='shopLink' to="/shopping-cart">
-                <img className='shopIcon' src='./shopping-outline.svg' />
-                {(itemQuantity>0) && <span className='cartNumber'>{(itemQuantity < 100) ? itemQuantity : '99+'}</span>}
-            </Link>
+            <Categories />
+            <div className='cart'>
+                <Link className='shopLink' to="/shopping-cart">
+                    <img className='shopIcon' src='./shopping-outline.svg' />
+                    {(itemQuantity>0) && <span className='cartNumber'>{(itemQuantity < 100) ? itemQuantity : '99+'}</span>}
+                </Link>
+            </div>
         </div>
     )
 }
